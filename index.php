5f60dd54a826cddb46b5b177b0006980acf5d379
Leo Lipowski <leonard.lipowski@gmail.com>
2022-04-16 00:08:53 +0200

    Implemented Mysql connection

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 header('Content-Type: application/json');
+
+//Load config file
+$c = include('config.php');
+
 //Get the request method from the $_SERVER
 //Print the request method out on to the page.
 
@@ -13,7 +17,7 @@ if(!$_GET['token']){
 
 //Checking if user has provided file name in GET
 if($_GET['file']){
-    echo $file_name = $_GET['file'];
+   //
 }else{
     $data = array("get_status"=>"failure", "file_name"=>"null", "error_code"=>"file_not_provided");
     header('Content-Type: application/json; charset=utf-8');
@@ -23,9 +27,21 @@ if($_GET['file']){
 
 
 
+//Create connection to the database
+$conn = new mysqli($c['server_address'], $c['username'], $c['password'], $c['db_name']);
+
+//Check if the connection has any errors
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+
+//If connection did not failed display information about connecting.
+}else{
+  echo "Connected successfully";
+}
 
 
-
+//End connection after running
+$conn->close();
 
 // if token exists in database
 // check is file
