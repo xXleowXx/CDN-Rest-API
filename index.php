660dcc325a23bbab5d39271c8466f41097701892
Leo Lipowski <leonard.lipowski@gmail.com>
2022-04-15 23:41:44 +0200

    Added Json Response

--- index.php
+++ index.php
@@ -1,42 +1,32 @@
 <?php
-
+header('Content-Type: application/json');
 //Get the request method from the $_SERVER
-
 //Print the request method out on to the page.
-$file_name = $_GET['file'];
-$access_token = '082DB07B60D5A99F8CEBEF3F12A2E13A'
+
+//Checking if user has provided token in GET
+if(!$_GET['token']){
+    $data = array("get_status"=>"failure", "token"=>"null", "error_code"=>"token_not_provided");
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($data, JSON_PRETTY_PRINT);
+    clean_all_processes();
+}
+
+//Checking if user has provided file name in GET
+if($_GET['file']){
+    echo $file_name = $_GET['file'];
+}else{
+    $data = array("get_status"=>"failure", "file_name"=>"null", "error_code"=>"file_not_provided");
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($data, JSON_PRETTY_PRINT);
+    clean_all_processes();
+}
+
+
+
+
+
 
 
 // if token exists in database
-// check is file coorelated with database
-
-
-if ($dummy_mysql_response){
-    $access_granted = true;
-}
-
-
-
-// Suppose your "public_html" folder is .
-$file = './../$file_name';
-$userCanDownloadThisFile = true; // apply your logic here
-
-
-if (file_exists($file) && $userCanDownloadThisFile) {
-    header('Content-Description: File Transfer');
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename=filename.gif');
-    header('Content-Transfer-Encoding: binary');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: ' . filesize($file));
-    ob_clean();
-    flush();
-    readfile($file);
-
-}
-
-
-
+// check is file
 ?>
