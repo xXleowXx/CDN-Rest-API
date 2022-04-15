a1e6d12a94cb921da7b24dac365aa4c107f325b6
Leo Lipowski <leonard.lipowski@gmail.com>
2022-04-16 01:55:34 +0200

    Test

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-    'production' => "live",
-    'server_address' => "localhost",
-    'username' => "leowlive_cdn",
-    'password' => "LLW!leow11",
-    'db_name' => "leowlive_cdn"
-);
